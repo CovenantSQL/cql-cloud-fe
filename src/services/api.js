bf29fd61b83d772baaa9cf6865176a14060e05e4
foreseaz <foreseazhang@gmail.com>
2019-06-11 11:29:34 +0800

    feat: add new apis

--- src/services/api.js
+++ src/services/api.js
@@ -12,20 +12,43 @@ export default {
   queryPostList: '/posts',
   queryDashboard: '/dashboard',
 
-  // CQL APIs
+  // ----- CQL APIs -----
+  // Ref: https://github.com/CovenantSQL/CovenantSQL/blob/feature/adapter/cmd/cql-proxy/api/init.go
   queryGithubOAuth: '/admin/auth/authorize',
   queryToken: '/admin/auth/callback',
   queryAccount: '/admin/account',
   getPTC: 'POST /admin/account/apply',
   queryCQLUserInfo: '/admin/userinfo',
 
+  // keypair
   createWallet: 'POST /admin/keypair',
   deleteWallet: 'DELETE /admin/keypair',
   uploadWallet: 'POST /admin/keypair/upload',
   setMainWallet: 'POST /admin/keypair/main',
   downloadWallet: '/admin/keypair/:account',
 
+  // task
   queryTasks: '/admin/task', // {offset, limit, all=true}
   queryTask: '/admin/task/:id',
   killTask: 'DELETE /admin/task/:id',
+
+  // project
+  createProject: 'POST /admin/project',
+  queryProject: '/admin/project',
+  queryProjectUserList: '/admin/project/:db/user',
+  createPreRregisterProjectUser: 'POST /admin/project/:db/user',
+  queryProjectUser: '/admin/project/:db/user/:id',
+  updateProjectUser: 'PUT /admin/project/:db/user',
+  queryProjectConfig: '/admin/project/:db/config',
+  updateProjectMiscConfig: 'PUT /admin/project/:db/config/misc',
+  queryProjectOAuthCallback: '/admin/project/:db/oauth/:provider/callback',
+  updateProjectOAuthConifg: 'PUT /admin/project/:db/oauth/:provider',
+  queryProjectTables: '/admin/project/:db/table',
+  createProjectTable: 'POST /admin/project/:db/table',
+  addFieldToProjectTable: 'PATCH /admin/project/:db/table/:table',
+  queryProjectTableDetail: '/admin/project/:db/table/:table',
+  dropProjectTable: 'DELETE /admin/project/:db/table/:table',
+
+  // tmp
+  queryProjectAudits: '/admin/project/:db/audits',
 }
